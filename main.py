04b55a880c8075696a0e4df41ecc33253710547e
MaurasBraj <mauritz.almgren@gmail.com>
2026-04-08 13:29:36 +0200

    Answered question 1 and started on question 2

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ import matplotlib as mpl
 
 df = pd.read_csv('SMHI_Data.csv')
 
-print(df.to_string())
+df_helper = df.copy()
+df_helper['Datum'] = pd.to_datetime(df_helper['Datum'])
+df_helper['Month'] = df_helper['Datum'].dt.month
+df_helper['Day'] = df_helper['Datum'].dt.day
+
+seasonal_means = df_helper.groupby(['Month', 'Day', 'Tid (UTC)'])['Lufttemperatur'].transform('mean')
+
+df_filled = df.copy()
+
+df_filled['Lufttemperatur'] = df_filled['Lufttemperatur'].fillna(seasonal_means).round(1)
+
+if df_filled['Lufttemperatur'].isna().any():
+    daily_means = df_helper.groupby(['Month', 'Day'])['Lufttemperatur'].transform('mean')
+    df_filled['Lufttemperatur'] = df_filled['Lufttemperatur'].fillna(daily_means)
+
+
+print(df_filled.to_string())
